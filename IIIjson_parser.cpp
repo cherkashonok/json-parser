d48734dcb7c3e926e7953dec6b4b57476d9fb8af
cherkash <lexichlexa@gmail.com>
2026-02-12 18:36:10 +0300

    полурабочая версия

--- IIIjson_parser.cpp
+++ IIIjson_parser.cpp
@@ -1,11 +1,145 @@
-#include <algorithm>
-#include <string>
-#include <iostream>
-
 #include "IIIjson_parser.hpp"
 
 
-int main() {
-    jsonValue t1(30, integer);
+// class jsonValue 
+// реализация getValue
+template<> 
+void* jsonValue::getValue<void*>() const {
+    return nullptr; 
+}
+
+template<> 
+std::string jsonValue::getValue<std::string>() const {
+    return j1; 
+}
+
+template<> 
+int jsonValue::getValue<int>() const {
+    return j2; 
+}
+
+template<> 
+bool jsonValue::getValue<bool>() const {
+    return j3; 
+}
+
+template<> 
+std::vector<jsonValue> jsonValue::getValue<std::vector<jsonValue>>() const {
+    return j4; 
+}
+
+template<> 
+JSON* jsonValue::getValue<JSON*>() const {
+    return j5; 
+}
+
+// реализация конструкторов
+template<> 
+jsonValue::jsonValue<std::string>(const std::string& val) {
+    type = string;
+    j1 = val;
+}
+
+template<> 
+jsonValue::jsonValue<int>(const int& val) {
+    type = number;
+    j2 = val;
+}
+
+template<> 
+jsonValue::jsonValue<bool>(const bool& val) {
+    type = boolean;
+    j3 = val;
+}
+
+template<> 
+jsonValue::jsonValue<std::vector<jsonValue>>(const std::vector<jsonValue>& val) {
+    type = array;
+    j4 = val;
+}
+
+jsonValue::jsonValue(const char* val) {
+    type=string;
+    j1 = std::string(val);
+}
+
+jsonValue::jsonValue(JSON* val) {
+    type = json;
+    j5 = val;
+}
+
+// остальной функционал jsonValue
+jsonValue& jsonValue::operator= (const jsonValue& right) {
+    if (this == &right)
+        return *this;
+    
+    if (right.type == null) {
+        type = right.type;
+    } else if (right.type == string) {
+        type = right.type;
+        j1 = right.j1;
+    } else if (right.type == number) {
+        type = right.type;
+        j2 = right.j2;
+    } else if (right.type == boolean) {
+        type = right.type;
+        j3 = right.j3;
+    } else if (right.type == array) {
+        type = right.type;
+        j4 = right.j4;
+    } else if (right.type == json) {
+        type = right.type;
+        j5 = right.j5;
+    }
+    
+    return *this;
+}
+
+bool jsonValue::operator< (const jsonValue& right) const {
+    return sizeof(*this) < sizeof(right);
+}
+
+bool jsonValue::operator== (const jsonValue& right) const {
+    if (type != right.type) return false;
+
+    if (type == null) return    true;
+    if (type == string) return  j1 == right.j1;
+    if (type == number) return  j2 == right.j2;
+    if (type == boolean) return j3 == right.j3;
+    if (type == array) return   j4 == right.j4;
+    if (type == json) return    j5 == right.j5;
+}
+
+
+// class JSON
+JSON::JSON (const std::string& file_name) {
+
+}
+
+void JSON::get_JSON_from_file(const std::string& name) {
+
+}
+
+void JSON::write_JSON_to_file(const std::string& name) {
+
+}
+
+
+
+int main () {
+    jsonValue t1(30);
     std::cout << t1.getValue<int>() << "\n";
+
+
+    JSON example;
+
+    example.get_JSON_from_file("example.json");
+
+    example[30] = "gerr";
+    example["fd"] = true;
+    example[true] = 12424;
+
+    std::cout << example[30].getValue<std::string>() << "\n";
+    std::cout << example["fd"].getValue<bool>() << "\n";
+    std::cout << example[true].getValue<int>() << "\n";
 }
--- IIIjson_parser.hpp
+++ IIIjson_parser.hpp
@@ -1,8 +1,7 @@
-// version 1.1.1
 #ifndef IIIJSON_PARSER_HPP
 #define IIIJSON_PARSER_HPP
 
-#include <algorithm>
+#include <iostream>
 #include <fstream>
 #include <map>
 #include <string>
@@ -11,84 +10,53 @@
 class JSON;
 class jsonValue;
 
-
 enum jsonValueType {
     null,
     string,
-    integer,
+    number,
     boolean,
     array,
     json
 };
 
+
 class jsonValue {
 private:
-    jsonValueType        type;
-    std::string            j1;
-    int64_t                j2;
-    bool                   j3;
-    std::vector<jsonValue> j4;
-    JSON                   j5;
+    jsonValueType         type;
+    std::string             j1;
+    int                     j2;
+    bool                    j3;
+    std::vector<jsonValue>  j4;
+    JSON*                   j5;
 public:
-    template<class T>
-    jsonValue(const T& val) {
-        switch (type) {
-            case string:
-                j1 = val; break;
-            case integer:
-                j2 = val; break;
-            case boolean:
-                j3 = val; break;
-            case array:
-                j4 = val; break;
-            case json:
-                j5 = val;
-        }
+    template<class T> jsonValue(const T& val);
+    jsonValue(const char* val);
+    jsonValue(JSON* val);
+    jsonValue() {}
+
+    template<class T> T getValue() const { 
+        throw "unknown type"; 
     }
 
-    template<class T>
-    jsonValue(const T& val, jsonValueType type) : type(type) {
-        jsonValue(val);
-    }
-
-    template<class T>
-    T getValue() {
-        switch (type) {
-            case null: 
-                return nullptr; break;
-            case string:
-                return j1; break
-            case integer:
-                return j2; break
-            case boolean:
-                return j3; break
-            case array:
-                return j4; break
-            case json:
-                return j5;
-        }
-    }
-
-    jsonValue (const jsonValue& parser) = delete;
-    jsonValue& operator= (const jsonValue& parser) = delete;
+    jsonValue& operator= (const jsonValue& right);
+    bool operator< (const jsonValue& right) const;
+    bool operator== (const jsonValue& right) const;
 };
 
-
-// nf
 class JSON {
 private:
     std::map<jsonValue, jsonValue> obj;
 public:
     JSON (const std::string& file_name);
+    JSON () {}
 
-    JSON (const JSON& parser) = delete;
-    JSON& operator= (const JSON& parser) = delete;
+    jsonValue& operator[] (const jsonValue& x) {
+        return obj[x];
+    }
+
+    void get_JSON_from_file(const std::string& name);
+    void write_JSON_to_file(const std::string& name);
 };
 
-// nf
-JSON get_JSON_from_file(const std::string& name);
-void write_JSON_to_file(const std::string& name);
 
-// спиздить или разработать больше функционала
-
-#endif
+#endif 
